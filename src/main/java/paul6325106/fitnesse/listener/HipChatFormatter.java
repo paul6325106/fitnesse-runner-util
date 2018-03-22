868cf6576fabb555b9515b2f9108c39eb23a2d29
paul6325106 <paul6325106@gmail.com>
2018-03-22 22:45:42 +1000

    Listener for notifying HipChat room

---
+++ src/main/java/paul6325106/fitnesse/listener/HipChatFormatter.java
@@ -0,0 +1,120 @@
+package paul6325106.fitnesse.listener;
+
+import fitnesse.testsystems.Assertion;
+import fitnesse.testsystems.ExceptionResult;
+import fitnesse.testsystems.TestPage;
+import fitnesse.testsystems.TestResult;
+import fitnesse.testsystems.TestSummary;
+import fitnesse.testsystems.TestSystem;
+import fitnesse.testsystems.TestSystemListener;
+import org.apache.http.Consts;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Reports test completion to a HipChat room.
+ */
+public class HipChatFormatter implements TestSystemListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HipChatFormatter.class);
+
+    private final HttpClient client;
+    private final String roomNotificationUrl;
+    private final String authKey;
+
+    public HipChatFormatter(final HttpClient client, final String roomNotificationUrl, final String authKey) {
+        this.client = client;
+        this.roomNotificationUrl = roomNotificationUrl;
+        this.authKey = authKey;
+    }
+
+    @Override
+    public void testComplete(final TestPage testPage, final TestSummary testSummary) {
+        if (isIgnoredPage(testPage)) {
+            return;
+        }
+
+        final JSONObject content = new JSONObject();
+        content.put("message", getMessage(testPage, testSummary));
+        content.put("color", getColour(testSummary));
+        content.put("notify", false);
+        content.put("message_format", "text");
+
+        final HttpPost post = new HttpPost(roomNotificationUrl);
+        post.addHeader("Authorization", "Bearer " + authKey);
+        post.addHeader("Content-Type", "application/json");
+        post.setEntity(new StringEntity(content.toString(), Consts.UTF_8));
+
+        final HttpResponse response;
+        try {
+            response = client.execute(post);
+        } catch (final IOException e) {
+            LOG.error("Error when sending notification to HipChat", e);
+            return;
+        }
+
+        final int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode < 200 || statusCode > 300) {
+            LOG.error("Post to HipChat unsuccessful: " + response.getStatusLine().getReasonPhrase());
+        }
+    }
+
+    private boolean isIgnoredPage(final TestPage testPage) {
+        return testPage.getFullPath().endsWith("SuiteSetUp") || testPage.getFullPath().endsWith("SuiteTearDown");
+    }
+
+    private String getMessage(final TestPage testPage, final TestSummary testSummary) {
+        return String.format("%s\nRight: %d\tWrong: %d\tIgnores: %d\tExceptions: %d", testPage.getFullPath(),
+                testSummary.getRight(), testSummary.getWrong(), testSummary.getIgnores(), testSummary.getExceptions());
+    }
+
+    private String getColour(final TestSummary testSummary) {
+        if (testSummary.getExceptions() > 0) {
+            return "yellow";
+        } else if (testSummary.getWrong() > 0) {
+            return "red";
+        } else if (testSummary.getRight() > 0) {
+            return "green";
+        } else {
+            return "gray";
+        }
+    }
+
+    @Override
+    public void testSystemStarted(final TestSystem testSystem) {
+        // ignored
+    }
+
+    @Override
+    public void testOutputChunk(final String output) {
+        // ignored
+    }
+
+    @Override
+    public void testStarted(final TestPage testPage) {
+        // ignored
+    }
+
+    @Override
+    public void testSystemStopped(final TestSystem testSystem, final Throwable cause) {
+        // ignored
+    }
+
+    @Override
+    public void testAssertionVerified(final Assertion assertion, final TestResult testResult) {
+        // ignored
+    }
+
+    @Override
+    public void testExceptionOccurred(final Assertion assertion, final ExceptionResult exceptionResult) {
+        // ignored
+    }
+
+}
---
+++ src/test/java/paul6325106/fitnesse/listener/HipChatFormatterTest.java
@@ -0,0 +1,131 @@
+package paul6325106.fitnesse.listener;
+
+import fitnesse.testsystems.TestPage;
+import fitnesse.testsystems.TestSummary;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicStatusLine;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HipChatFormatterTest {
+
+    private HipChatFormatter hipChatFormatter;
+    private String authKey = "some auth key";
+
+    @Mock
+    private HttpClient client;
+
+    @Mock
+    private HttpResponse response;
+
+    @Before
+    public void setUp() throws Exception {
+        hipChatFormatter = new HipChatFormatter(client, "http://www.someurl.com/", authKey);
+
+        when(client.execute(any(HttpPost.class))).thenReturn(response);
+        when(response.getStatusLine()).thenReturn(new BasicStatusLine(HttpVersion.HTTP_1_1, 200, null));
+    }
+
+    private void testComplete(final String fullPath, final int right, final int wrong, final int ignores,
+            final int exceptions) {
+
+        final TestPage testPage = mock(TestPage.class);
+        when(testPage.getFullPath()).thenReturn(fullPath);
+
+        hipChatFormatter.testComplete(testPage, new TestSummary(right, wrong, ignores, exceptions));
+    }
+
+    private void assertPost(final HttpPost post, final String colour, final String message) throws IOException {
+        assertEquals("Bearer " + authKey, post.getFirstHeader("Authorization").getValue());
+        assertEquals("application/json", post.getFirstHeader("Content-Type").getValue());
+
+        final JSONObject json;
+        try (final InputStream content = post.getEntity().getContent()) {
+            json = new JSONObject(IOUtils.toString(content, StandardCharsets.UTF_8));
+        }
+
+        assertEquals("text", json.getString("message_format"));
+        assertEquals(colour, json.getString("color"));
+        assertEquals(message, json.getString("message"));
+        assertFalse(json.getBoolean("notify"));
+    }
+
+    @Test
+    public void testIgnoredPages() throws Exception {
+        testComplete("WubWubWub.SuiteSetUp", 1, 1, 1, 1);
+        verifyNoMoreInteractions(client);
+
+        testComplete("WubWubWub.SuiteTearDown", 1, 1, 1, 1);
+        verifyNoMoreInteractions(client);
+
+        testComplete("WubWubWub.SuiteTearDownA", 1, 1, 1, 1);
+        verify(client, times(1)).execute(any(HttpPost.class));
+
+        testComplete("WubWubWub.SuiteTearDown.WubWubWub", 1, 1, 1, 1);
+        verify(client, times(2)).execute(any(HttpPost.class));
+    }
+
+    @Test
+    public void testRight() throws Exception {
+        testComplete("TestPage", 5, 0, 5, 0);
+
+        final ArgumentCaptor<HttpPost> postCapture = ArgumentCaptor.forClass(HttpPost.class);
+        verify(client, times(1)).execute(postCapture.capture());
+
+        assertPost(postCapture.getValue(), "green", "TestPage\nRight: 5\tWrong: 0\tIgnores: 5\tExceptions: 0");
+    }
+
+    @Test
+    public void testWrong() throws Exception {
+        testComplete("TestPage", 5, 1, 5, 0);
+
+        final ArgumentCaptor<HttpPost> postCapture = ArgumentCaptor.forClass(HttpPost.class);
+        verify(client, times(1)).execute(postCapture.capture());
+
+        assertPost(postCapture.getValue(), "red", "TestPage\nRight: 5\tWrong: 1\tIgnores: 5\tExceptions: 0");
+    }
+
+    @Test
+    public void testIgnores() throws Exception {
+        testComplete("TestPage", 0, 0, 0, 0);
+
+        final ArgumentCaptor<HttpPost> postCapture = ArgumentCaptor.forClass(HttpPost.class);
+        verify(client, times(1)).execute(postCapture.capture());
+
+        assertPost(postCapture.getValue(), "gray", "TestPage\nRight: 0\tWrong: 0\tIgnores: 0\tExceptions: 0");
+    }
+
+    @Test
+    public void testExceptions() throws Exception {
+        testComplete("TestPage", 5, 5, 5, 1);
+
+        final ArgumentCaptor<HttpPost> postCapture = ArgumentCaptor.forClass(HttpPost.class);
+        verify(client, times(1)).execute(postCapture.capture());
+
+        assertPost(postCapture.getValue(), "yellow", "TestPage\nRight: 5\tWrong: 5\tIgnores: 5\tExceptions: 1");
+    }
+
+}
